04480cedca6113d78bf85293e7ce0045ce70f14f
hiro1117 <hiro1117@gmail.com>
2025-06-05 21:42:44 +0900

    update prisma client

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,23 +1,17 @@
 "use server";
-import { PrismaClient } from "@/lib/generated/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { prisma } from "@/db/prisma";
+import { convertToPlainObject } from "../utils";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   // Note:behavior - LATEST_PRODUCTS_LIMIT is a constant that is used to limit the number of products returned by the getLatestProducts function.
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
-  return data.map((product) => ({
-    ...product,
-    price: product.price.toString(),
-    rating: product.rating.toString(),
-  }));
+  return convertToPlainObject(data);
 }
 
 // Get single product by it's slug
